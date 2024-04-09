041c4c4284a46ee744e78d0525c5b179dca8a4d3
Alauddin <alauddin3604@gmail.com>
2024-04-09 11:53:57 +0800

    Add `store` and `destroy` method for `User`

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\RolesEnum;
+use App\Http\Requests\StoreUserRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
 use App\Traits\PaginateTrait;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Http\Response;
 
 class UserController extends Controller
 {
@@ -23,9 +26,16 @@ class UserController extends Controller
         return UserResource::collection($this->paginateOrGet($user));
     }
 
-    public function store(Request $request)
+    public function store(StoreUserRequest $request): Response
     {
-        
+        $input = $request->validated();
+        $input['password'] = bcrypt('password');
+
+        $user = User::create($input);
+
+        $user->assignRole(RolesEnum::EMPLOYEE);
+
+        return response()->noContent(Response::HTTP_CREATED);
     }
 
     public function show(User $user)
@@ -38,8 +48,14 @@ class UserController extends Controller
         
     }
 
-    public function destroy(User $user)
+    public function destroy(User $user): Response
     {
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+        $userCannotBeDeleted = $user->isAdmin() || ($user->id === auth()->id());
+
+        abort_if($userCannotBeDeleted, Response::HTTP_FORBIDDEN, 'User cannot be deleted.');
+
+        $user->delete();
+
+        return response()->noContent();
     }
 }
