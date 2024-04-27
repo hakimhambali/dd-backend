605e745ee9bc1b7d4f61e2a121f50f596e2b7485
Alauddin <alauddin3604@gmail.com>
2024-04-27 23:33:36 +0800

    Create gender enum

---
+++ app/Enums/Gender.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Enums;
+
+enum Gender: string
+{
+    case MALE = 'male';
+    case FEMALE = 'female';
+
+    public static function values(): array
+    {
+        return array_column(self::cases(), 'value');
+    }
+
+    public function label(): string
+    {
+        return ucfirst($this->value);
+    }
+}
